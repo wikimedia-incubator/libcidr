99def3e616ceb28f0f70cdc0330186e906e33fb6
Matthew Fuller <fullermd@over-yonder.net>
2005-12-17 03:02:28 -0600

    Implement showing the address and netmask in binary format upon request.

--- src/examples/cidrcalc/cidrcalc.c
+++ src/examples/cidrcalc/cidrcalc.c
@@ -12,6 +12,65 @@
 /* Width for the line defs */
 #define DWID 9
 
+/* Gen up an octet in binary */
+#define OCTET_BIN(oct) \
+			{ \
+				memset(boct, 0, 9); \
+				for(obi = 7 ; obi>=0 ; obi--) \
+					if( ((oct >> obi) & 1) == 1) \
+						boct[7-obi] = '1'; \
+					else \
+						boct[7-obi] = '0'; \
+			}
+
+/* Show binary form of something */
+#define SHOWBIN(pt, pname) \
+			{ \
+				printf("%*s:", DWID, "Bin" pname); \
+				if(proto==CIDR_IPV4) \
+				{ \
+					/* Show v4 inline */ \
+					for(i=12 ; i<=15 ; i++) \
+					{ \
+						OCTET_BIN(addr->pt[i]) \
+						printf(" %s", boct); \
+					} \
+ 	 	 	 	 	\
+					/* Now skip to the same starting point */ \
+					printf("\n%*s ", DWID, ""); \
+ 	 	 	 	 	\
+					/* And show the decimal octets below */ \
+					for(i=12 ; i<=15 ; i++) \
+						printf(" %5d%3s", addr->pt[i], ""); \
+					printf("\n"); \
+				} \
+				else if(proto==CIDR_IPV6) \
+				{ \
+					/* v6 needs to span multiple lines */ \
+					for(i=0 ; i<=3 ; i++) \
+					{ \
+						/* 4 octets in binary */ \
+						for(j=i*4 ; j<=(i*4)+3 ; j++) \
+						{ \
+							OCTET_BIN(addr->pt[j]) \
+							printf(" %s", boct); \
+						} \
+						\
+						/* Those 4 octets in hex */ \
+						printf("\n%*s ", DWID, ""); \
+						for(j=i*4 ; j<=i*4+3 ; j++) \
+							printf("    %.2x   ", addr->pt[j]); \
+						\
+						/* Prep for next round */ \
+						if(i<3) \
+							printf("\n%*s ", DWID, ""); \
+						else \
+							printf("\n"); \
+					} \
+				} \
+			}
+
+/* Globals/prototypes */
 char *pname;
 void usage(void);
 
@@ -21,6 +80,8 @@ main(int argc, char *argv[])
 	CIDR *addr, *addr2, *addr3;
 	char *astr, *astr2;
 	char boct[9];
+	int obi;
+	int i, j;
 	const char *cstr;
 	int goch;
 	short proto;
@@ -28,7 +89,6 @@ main(int argc, char *argv[])
 
 	pname = *argv;
 	showbin = 0;
-	memset(boct, 0, 9);
 
 	while((goch=getopt(argc, argv, "b"))!=-1)
 	{
@@ -62,9 +122,11 @@ main(int argc, char *argv[])
 			/* Start putting out the pieces */
 			proto = addr->proto;
 
+			/* Address */
 			astr = cidr_to_str(addr, CIDR_ONLYADDR);
 			printf("%*s: %s\n", DWID, "Address", astr);
 			free(astr);
+
 
 			/* Show the full 'expanded' address form */
 			if(proto==CIDR_IPV6)
@@ -75,25 +137,32 @@ main(int argc, char *argv[])
 				free(astr);
 			}
 
-			/* Show binary? */
-			if(showbin==1)
-			{
-				printf("%*s: ", DWID, "BinAddr");
-				printf("\n%*s  WRITEME\n", DWID, "");
-			}
 
+			/* Netmask */
 			astr = cidr_to_str(addr, CIDR_ONLYPFLEN);
 			astr2 = cidr_to_str(addr, CIDR_ONLYPFLEN | CIDR_NETMASK);
 			printf("%*s: %s (/%s)\n", DWID, "Netmask", astr2, astr);
 			free(astr);
 			free(astr2);
 
+
+			/* Show binary forms? */
+			if(showbin==1)
+			{
+				SHOWBIN(addr, "Addr")
+				SHOWBIN(mask, "Mask")
+			}
+
+
+			/* Wildcard mask */
 			astr = cidr_to_str(addr,
 					CIDR_ONLYPFLEN | CIDR_NETMASK | CIDR_WILDCARD);
 			/* Spaced to match above */
 			printf("%*s: %s\n", DWID, "Wildcard", astr);
 			free(astr);
 
+
+			/* Network and broadcast */
 			addr2 = cidr_addr_network(addr);
 			astr = cidr_to_str(addr2, CIDR_NOFLAGS);
 			printf("%*s: %s\n", DWID, "Network", astr);
@@ -106,6 +175,8 @@ main(int argc, char *argv[])
 			free(astr);
 			cidr_free(addr2);
 
+
+			/* Range of hosts */
 			addr2 = cidr_addr_hostmin(addr);
 			astr = cidr_to_str(addr2, CIDR_ONLYADDR);
 			addr3 = cidr_addr_hostmax(addr);
@@ -116,9 +187,12 @@ main(int argc, char *argv[])
 			cidr_free(addr2);
 			cidr_free(addr3);
 
+
+			/* Num of hosts */
 			cstr = cidr_numhost(addr);
 			printf("%*s: %s\n", DWID, "NumHosts", cstr);
 			/* Don't free cstr */
+
 
 			/* That's it for this address */
 			cidr_free(addr);
