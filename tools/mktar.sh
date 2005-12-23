07658a6730e099fe958e296d49e5ee3c78c4b40b
Matthew Fuller <fullermd@over-yonder.net>
2005-12-23 11:50:14 -0600

    Add a quick&dirty script to create a dir suitable for tar'ing and releasing.

---
+++ tools/mktar.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Quick hack of a tool to make a pristine tarball of libcidr
+# Minimal seatbelts
+
+dstdir=$1
+
+if [ "X${dstdir}" = "X" ]; then
+	echo "You better tell me where to put it, buddy!"
+	exit
+fi
+
+mkdir -p ${dstdir}/.bzr
+tar -cf - -C ../.bzr/ . | tar -xpf - -C ${dstdir}/.bzr
+(cd ${dstdir} && bzr revert ; sh mkgmake.sh)
+(cd ${dstdir}/docs/reference/sgml/ && make all clean)
+(cd ${dstdir}/include && \
+	revid=`bzr testament | grep ^revision-id | awk '{print $2}'` ; \
+	sed -i "" -e "s/CIDR_REVISION \"\"/CIDR_REVISION \" (${revid})\"/" \
+			libcidr.h )
+(cd ${dstdir} && rm -rf .bzr .bzrignore tools/mktar.sh)
